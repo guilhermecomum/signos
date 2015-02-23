64c5865a65adbd0815a1b262140b17a5584ca487
Guilherme Guerra <guerrinha@comum.org>
2015-02-23 10:10:26 -0300

    Initial vizualization with csv sample

--- app/assets/javascripts/app/home.js
+++ app/assets/javascripts/app/home.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
 
-  var signos = ["Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn","Aquarius","Pisces"]
-  var dataset = [];
-  for (var i = 0; i < 12; i++) {
-    var newNumber = Math.random() * 30;
-    dataset.push(newNumber);
-  }
+  d3.csv("/assets/pnad.csv", function(dataset) {
+    var columns = ["signo", "renda"];
 
-  d3.select(".magic").selectAll("div")
-    .data(dataset)
-    .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", function(d){
-      var barHeight = d * 5;
-      return barHeight + "px";
-    })
-    .style("width", function(d){
-      var barHeight = d * 5;
-      return barHeight + "px";
+
+
+    d3.select(".stage").selectAll("div")
+      .data(dataset)
+      .enter()
+      .append("div")
+      .text(function(column) { return column["signo"]; })
+      .attr("class", "bar")
+      .style("height", function(column){
+        var barHeight = column["renda"] * 50;
+        return barHeight + "px";
+      })
+      .style("width", function(column){
+        var barHeight = column["renda"] * 50;
+        return barHeight + "px";
+      });
+
     });
-
 });
